c4f0248c6a61e9a76f7c4bf2fe997b9051191198
ptflp <filippov.pa@s-vfu.ru>
2018-07-22 17:47:16 +0900

    small changes

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -1,40 +1,38 @@
 <?php
-if(!empty(REQURL[1])) {
-	switch (REQURL[1]) {
-		case 'login':
-			if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
-				$user = new User($entity_manager);
-				$auth=$user->auth($_REQUEST['email'],$_REQUEST['password']);
-				if ($auth) {
-					$user->authorize($user->id);
-				} else {
-					$error['error']='Неправильная пара логин пароль';
-					echo json_encode($error,JSON_UNESCAPED_UNICODE);
-				}
+switch (REQURL[1]) {
+	case 'login':
+		if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
+			$user = new User($entity_manager);
+			$auth=$user->auth($_REQUEST['email'],$_REQUEST['password']);
+			if ($auth) {
+				$user->authorize($user->id);
+			} else {
+				$error['error']='Неправильная пара логин пароль';
+				echo json_encode($error,JSON_UNESCAPED_UNICODE);
 			}
-		break;
-		case 'logout':
-			session_destroy();
-			header('location: /user/login');
-		break;
-		case 'register':
-			if (!empty($_SESSION['uid'])) {
-				header('location: /');
-			}
-			if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
-				$user= new User();
-				if ($user->register($_REQUEST['email'],$_REQUEST['password'])) {
-					$userN=$entity_manager->getRepository('entities\User')->findOneBy(['email' => $_REQUEST['email']]);
- 					$user->authorize($userN->getId());
-				} else {
-					$error['error']='данный пользователь уже существует: '.$_REQUEST['email'];
-					echo json_encode($error,JSON_UNESCAPED_UNICODE);
-				}
-			}
-		break;
-
-		default:
+		}
+	break;
+	case 'logout':
+		session_destroy();
+		header('location: /user/login');
+	break;
+	case 'register':
+		if (!empty($_SESSION['uid'])) {
 			header('location: /');
-		break;
-	}
+		}
+		if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
+			$user= new User();
+			if ($user->register($_REQUEST['email'],$_REQUEST['password'])) {
+				$userN=$entity_manager->getRepository('entities\User')->findOneBy(['email' => $_REQUEST['email']]);
+					$user->authorize($userN->getId());
+			} else {
+				$error['error']='данный пользователь уже существует: '.$_REQUEST['email'];
+				echo json_encode($error,JSON_UNESCAPED_UNICODE);
+			}
+		}
+	break;
+
+	default:
+		require('404.php');
+	break;
 }
