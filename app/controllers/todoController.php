a2bc5c174f16ad2a34935435f5f4bac483083feb
ptflp <filippov.pa@s-vfu.ru>
2018-07-22 21:43:08 +0900

    add func creating todolist

--- app/controllers/todoController.php
+++ app/controllers/todoController.php
@@ -2,7 +2,7 @@
 if (empty($_SESSION['uid'])) {
 	header('location: /user/login');
 } else {
-	$user=new User();
+	$main_user=new User();
 }
 
 switch (REQURL[1]) {
@@ -10,22 +10,28 @@ switch (REQURL[1]) {
 	break;
 	case 'create':
 		if (isset($_REQUEST['title'])) {
-			$user = $entity_manager->getRepository('entities\User')->findOneBy(['id' => $user->id]);
-			$entity_manager->persist($user);
-			// Create a new task
-			$task = (new entities\Todolist())
-			    ->setTitle($_REQUEST['title']);
-			    ->setTasks("[{}]");
-			    ->setUser($user)
-			    ->setDate(new DateTime());
+			try {
+				$user = $entity_manager->getRepository('entities\User')->findOneBy(['id' => $main_user->id]);
+				$entity_manager->persist($user);
+				// Create a new task
+				$task = (new entities\Todolist())
+				    ->setTitle($_REQUEST['title'])
+				    ->setTasks("[{}]")
+				    ->setUser($user)
+				    ->setDate(new DateTime());
 
-			// Add the task the to list of the User tasks. Since we used cascade={"all"}, we
-			// don't need to persist the task separately: it will be persisted when persisting
-			// the User
-			$user->addTodolist($task);
+				// Add the task the to list of the User tasks. Since we used cascade={"all"}, we
+				// don't need to persist the task separately: it will be persisted when persisting
+				// the User
+				$user->addTodolist($task);
 
-			// Finally flush and execute the database transaction
-			$entity_manager->flush();
+				// Finally flush and execute the database transaction
+				$entity_manager->flush();
+			} catch (Doctrine\DBAL\DBALException $e) {
+				$msg['success']=0;
+				$msg['error']=$e;
+				echo json_encode($msg,JSON_UNESCAPED_UNICODE);
+			}
 		} else {
 			header('location: /');
 		}
@@ -33,6 +39,10 @@ switch (REQURL[1]) {
 	case 'getlist':
 	break;
 	default:
-		header('location: /');
+		if (is_numeric($_REQUEST['id'])) {
+			require('../view/todo.html');
+		} else {
+			header('location: /');
+		}
 	break;
 }
