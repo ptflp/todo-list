c6221e785cefabad3f73c01b214eb2bd6f6744fc
ptflp <filippov.pa@s-vfu.ru>
2018-07-31 15:53:15 +0900

    slim index controller

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -9,30 +9,26 @@ class IndexController extends AppController
 	public function actionIndex()
 	{
 		$todo=new Todo;
-		$todoList=$todo->getUserTodo($TodoApp->user->db);
-		$sharedList=$todo->getShared($TodoApp->user->db,$TodoApp->db);
+		$todoList=$todo->getUserTodo($this->user->id);
+		$sharedList=$todo->getShared($this->user->id);
+
+		$sharedList=$this->addCountItem($sharedList);
+		$todoList=$this->addCountItem($todoList);
+
+		$content['todoList']=$todoList;
+		$content['sharedList']=$sharedList;
+		$content['email']=$this->user->email;
+		echo $this->view->muRender('index',$content);
+	}
+	public function addCountItem($array)
+	{
 		$i=2;
-		foreach ($sharedList as $key=>$value){
-			$b=0;
+		foreach ($array as $key=>$value){
 			if ($i>12) {
 				$i=2;
 			}
-			if ($i>9){$b='';}
-			$sharedList[$key]['count']=$b.$i++;
+			$array[$key]['count']=$i++;
 		}
-		$i=2;
-		foreach ($todoList as $key=>$value){
-			$b=0;
-			if ($i>12) {
-				$i=2;
-			}
-			if ($i>9){$b='';}
-			$todoList[$key]['count']=$b.$i++;
-		}
-		$index['todoList']=$todoList;
-		$index['sharedList']=$sharedList;
-		$index['email']=$TodoApp->user->email;
-		$view = $TodoApp->mustache->loadTemplate('index');
-		echo $view->render($index);
+		return $array;
 	}
 }
