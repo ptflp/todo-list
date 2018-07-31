c5b123702ae0a31f36d6a242957ea7085e1627f2
ptflp <filippov.pa@s-vfu.ru>
2018-07-31 17:01:48 +0900

    notfound added

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -14,5 +14,14 @@ class AppController extends Controller
 		session_start();
         $this->user = new User;
 	}
+	public function notFound()
+	{
+		header("HTTP/1.0 404 Not Found");
+		header("HTTP/1.1 404 Not Found");
+		header("Status: 404 Not Found");
+		$content['message'] = "404 Not Found";
+		$this->view->layout='404';
+ 		echo $this->view->muRender('404',$content);
+	}
 
 }
