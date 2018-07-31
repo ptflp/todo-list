d37e45c936e14905c9e096075c6c5f32e50d6bfa
ptflp <filippov.pa@s-vfu.ru>
2018-07-31 10:50:54 +0900

    restrict app controller

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,6 +3,8 @@
 * Example searching rule regexp '([a-z])' => 'news/view' replace path name to change controller
 */
 return Array (
+	'app$' => 'user/login',
+	'app\/.*' => 'user/login',
 	'news\/([a-z]+)\/([0-9]+)$' => 'news/view/$2/$1',
 	'news\/([0-9]+)$' => 'news/view/$1',
 	'news$' => 'news/index',
