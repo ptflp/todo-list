af4546c0afe09bcb4ca4062a6f2329f5e4a83bc9
ptflp <filippov.pa@s-vfu.ru>
2018-08-01 12:13:22 +0900

    remove default routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -6,7 +6,4 @@ return Array (
 	'todo\/([0-9]+)$' => 'todo/index/$1',
 	'app$' => 'error/index',
 	'app\/.*' => 'error/index',
-	'news\/([a-z]+)\/([0-9]+)$' => 'news/view/$2/$1',
-	'news\/([0-9]+)$' => 'news/view/$1',
-	'news$' => 'news/index',
 );
