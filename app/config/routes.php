91e8396823df3d9d993ad6804013e39ac04d68cf
ptflp <filippov.pa@s-vfu.ru>
2018-08-01 12:20:00 +0900

    add comments

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,7 +3,7 @@
 * Example searching rule regexp '([a-z])' => 'news/view' replace path name to change controller
 */
 return Array (
-	'todo\/([0-9]+)$' => 'todo/index/$1',
-	'app$' => 'error/index',
-	'app\/.*' => 'error/index',
+	'todo\/([0-9]+)$' => 'todo/index/$1', // todo routing law
+	'app$' => 'error/index', // closing service controller
+	'app\/.*' => 'error/index', // closing service controller
 );
